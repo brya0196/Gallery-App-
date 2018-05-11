afde39002e78eb4deb0f5d1934213c58c915bae1
Bryan Astacio <brya0196@gmail.com>
2018-05-11 18:45:47 -0400

    Added the services layer and implemented the ImageService to the gallery controller

---
+++ SimpleImageGallery.Data/IImage.cs
@@ -0,0 +1,12 @@
+﻿using SimpleImageGallery.Data.Models;
+using System.Collections.Generic;
+
+namespace SimpleImageGallery.Data
+{
+    public interface IImage
+    {
+        IEnumerable<GalleryImage> GetAll();
+        IEnumerable<GalleryImage> GetWithTag(string Tag);
+        GalleryImage GetById(int id);
+    }
+}
--- SimpleImageGallery/Controllers/GalleryController.cs
+++ SimpleImageGallery/Controllers/GalleryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SimpleImageGallery.Data;
 using SimpleImageGallery.Data.Models;
 using SimpleImageGallery.Models;
 
@@ -10,56 +11,16 @@ namespace SimpleImageGallery.Controllers
 {
     public class GalleryController : Controller
     {
+        private readonly IImage _imageService;
+
+        public GalleryController(IImage imageService)
+        {
+            _imageService = imageService;
+        }
+
         public IActionResult Index()
         {
-            var hikingImageTag = new List<ImageTag>();
-            var cityImageTag = new List<ImageTag>();
-
-            var tag1 = new ImageTag()
-            {
-                Description = "Adventure",
-                Id = 0
-            };
-
-            var tag2 = new ImageTag()
-            {
-                Description = "Urban",
-                Id = 1
-            };
-
-            var tag3 = new ImageTag()
-            {
-                Description = "New york",
-                Id = 2
-            };
-
-            hikingImageTag.Add(tag1);
-            cityImageTag.AddRange(new List<ImageTag> { tag2, tag3 });
-
-            var imageList = new List<GalleryImage>()
-            {
-                new GalleryImage()
-                {
-                    Title = "Hiking Trip",
-                    Url = "https://images.pexels.com/photos/771079/pexels-photo-771079.jpeg",
-                    Created = DateTime.Now,
-                    Tags = hikingImageTag
-                },
-                new GalleryImage()
-                {
-                    Title = "On the Trail",
-                    Url = "https://images.pexels.com/photos/868097/pexels-photo-868097.jpeg",
-                    Created = DateTime.Now,
-                    Tags = hikingImageTag
-                },
-                new GalleryImage()
-                {
-                    Title = "Downtown",
-                    Url = "https://images.pexels.com/photos/378570/pexels-photo-378570.jpeg",
-                    Created = DateTime.Now,
-                    Tags = cityImageTag
-                }
-            };
+            var imageList = _imageService.GetAll();
 
             var model = new GalleryIndexModel()
             {
